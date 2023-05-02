00ee2b9960225527665325fe4310f5f8800510a9
vyas7 <vyasaraj7@gmail.com>
2023-05-02 23:31:13 +0200

    modified Linkedlist file

--- LinkedList.py
+++ LinkedList.py
@@ -16,13 +16,30 @@ class LinkedList:
         if self.head is None:
             self.head = add_node
             self.tail = add_node
-
         else:
             self.tail.next = add_node
             self.tail = add_node
 
         self.length += 1
         return True
+
+    def pop(self):
+        if self.head is None:
+            return False
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return True
+        temp = self.head
+        while temp is not None:
+            if temp.next.next is None:
+                self.tail = temp
+                self.tail.next = None
+                self.length -= 1
+                return True
+            temp = temp.next
+        return False
 
     def prepend(self, value):
         new_node = Node(value)
@@ -46,5 +63,11 @@ ll1 = LinkedList(5)
 ll1.append(10)
 ll1.append(20)
 ll1.append(30)
+ll1.append(0)
+ll1.pop()
+ll1.pop()
+ll1.pop()
+ll1.pop()
+ll1.pop()
 ll1.print_list()
 print(ll1.length)
